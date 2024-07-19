ceb29422ef92c6b0a5620c6e88927a2787884389
kez <kerrialbeckettnewham@gmail.com>
2024-07-19 22:18:10 +0200

    init commit

---
+++ src/app/components/ProductCart.tsx
@@ -0,0 +1,15 @@
+'use client'
+import React from 'react'
+
+const ProductCart = () => {
+    return (
+        <div>
+            <button onClick={() => {
+                console.log('clicked!!!!')
+            }}>Add To Cart
+            </button>
+        </div>
+    );
+}
+
+export default ProductCart
